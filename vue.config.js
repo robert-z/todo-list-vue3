6c4537626dac1f8abc8c232e2708cc6efe068421
robert-z <robertz.perez1985@gmail.com>
2020-11-23 12:34:03 +0100

    Create new components alias

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,12 @@ const path = require('path');
 
 module.exports = {
   configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'src'),
+        '@components': path.join(__dirname, 'src', 'ui', 'components')
+      }
+    },
     entry: {
       app: path.join(__dirname, 'src', 'ui', 'main.ts')
     }
